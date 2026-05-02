ca22a97a807b6ef3a5851897d1b0d7a9e716ef5e
neurxdreamz <kononovs774@mail.ru>
2026-05-02 16:09:04 +0300

    Начал разработку калькулятора юнит экономики

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace TradeNodeLogic
+{
+    public class CalculatorResult
+    {
+        public decimal TurnOver { get; set; }
+        public decimal TotalExpenses { get; set; }
+        public decimal NetProfit { get; set; }
+        public decimal Margin { get; set; }
+    }
+
+    public static class AvitoCalculator
+    {
+        public static CalculatorResult Calculate (decimal cost, decimal quantity, decimal price, decimal CommissionPercent, decimal adsCost)
+        {
+            decimal turnover = price * quantity;
+            decimal totalCostPrice = cost * quantity;
+            decimal commissionAbsolute = turnover * (CommissionPercent / 100m);
+            decimal totalExpenses = totalCostPrice + commissionAbsolute + adsCost;
+            decimal netProfit = turnover - totalExpenses;
+
+            decimal margin = 0;
+
+            if (turnover > 0)
+            {
+                margin = (netProfit / turnover) * 100m;
+            }
+
+            return new CalculatorResult
+            {
+                TurnOver = turnover,
+                TotalExpenses = totalExpenses,
+                NetProfit = netProfit,
+                Margin = Math.Round(margin, 2)
+            };
+
+
+        }
+    }
+}
---
+++ TradeNode/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TradeNodeLogic; // Подключаем нашу независимую логику!
+using TradeNode;
+using TradeNodeLogic;
+
+namespace TradeNode.UI.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private decimal _price;
+        public decimal Price { get => _price; set { _price = value; OnPropertyChanged(); } }
+
+        private decimal _cost;
+        public decimal Cost { get => _cost; set { _cost = value; OnPropertyChanged(); } }
+
+        private int _quantity;
+        public int Quantity { get => _quantity; set { _quantity = value; OnPropertyChanged(); } }
+
+        private decimal _commission = 5;
+        public decimal Commission { get => _commission; set { _commission = value; OnPropertyChanged(); } }
+
+        private decimal _adsCost;
+        public decimal AdsCost { get => _adsCost; set { _adsCost = value; OnPropertyChanged(); } }
+
+        private CalculatorResult _result = new CalculatorResult();
+
+        // Свойства для вывода на экран
+        public decimal Turnover => _result.TurnOver;
+        public decimal TotalExpenses => _result.TotalExpenses;
+        public decimal NetProfit => _result.NetProfit;
+        public decimal Margin => _result.Margin;
+
+        public ICommand CalculateCommand { get; }
+
+        public MainViewModel()
+        {
+            CalculateCommand = new RelayCommand(ExecuteCalculate);
+        }
+
+        private void ExecuteCalculate(object parameter)
+        {
+            // Вызываем расчет из библиотеки Core
+            _result = AvitoCalculator.Calculate(Price, Cost, Quantity, Commission, AdsCost);
+
+            // Сообщаем интерфейсу, что результаты обновились
+            OnPropertyChanged(nameof(Turnover));
+            OnPropertyChanged(nameof(TotalExpenses));
+            OnPropertyChanged(nameof(NetProfit));
+            OnPropertyChanged(nameof(Margin));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
